23d26b7d5c37579fc6c5829367c974e576189964
Ubuntu <ubuntu@ip-172-31-1-59.ec2.internal>
2026-07-08 09:32:07 +0000

    phase 6 worker2

--- worker/app/config.py
+++ worker/app/config.py
@@ -3,3 +3,8 @@ WORKER_ID = "worker1"
 AGGREGATOR_URL = "http://16.170.224.169:8000"
 
 DATASET_PATH = "../datasets/processed/worker1.csv"
+WORKER_ID = "worker2"
+
+AGGREGATOR_URL = "http://16.170.224.169:8000"
+
+DATASET_PATH = "../datasets/processed/worker2.csv"
